5ef2d07452a96185f70dd32642f2990165bdb753
Konstantin Sopin <sopinorg@gmail.com>
2018-11-25 14:56:43 +0000

    fix

--- cmd/gin_http_server/main.go
+++ cmd/gin_http_server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"auth/pkg/app"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 
 func main() {
@@ -25,6 +29,68 @@ func InitRoutes(engine *gin.Engine) {
 	engine.POST("/sign-out", signout)
 	engine.POST("/sign-up", signup)
 	engine.POST("/authenticated", isAuthenticated)
+}
+
+
+
+func signup(c *gin.Context) {
+	ok, err := app.GetAppService().SignUp()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": ok,
+	})
+}
+
+
+func signin(c *gin.Context) {
+	token, err := app.GetAppService().SignIn()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
+}
+
+func signout(c *gin.Context) {
+	err := app.GetAppService().SignOut()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{})
+}
+
+
+func isAuthenticated(c *gin.Context) {
+	ok, err := app.GetAppService().IsAuthenticated()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"authenticated": ok,
+	})
 }
 
 
